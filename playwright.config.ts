c47e06a48490af2ea4889b2ff8e96701f144bde6
Thakur <b.a.thakur@accenture.com>
2026-09-14 15:47:14 +0530

    My test cases

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,30 +18,31 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: 3,
+  //retries: 3,
 
   //retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+ // workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  
   use: {
     video: 'on',
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'https://demoqa.com',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    browserName: 'chromium',
+    channel: 'chrome',
+    headless: false,
     trace: 'on-first-retry',
   },
 
-  /* Configure projects for major browsers */
+ 
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
+/*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
